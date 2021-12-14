f2f0fb23cfecb7373f3d9202718205ab5b8caaf1
Natalie Jones <nnjones99@gmail.com>
2021-12-14 11:10:07 -0600

    Initial commit

---
+++ calc/Arithmetic.java
@@ -0,0 +1,35 @@
+package calc;
+
+public class Arithmetic {
+	public static float add(float num1, float num2) {
+		return num1 + num2;
+	}
+
+	public static float subtract(float num1, float num2) {
+		return num1 - num2;
+	}
+
+	public static float multiply(float num1, float num2) {
+		return num1 * num2;
+	}
+
+	public static float divide(float num1, float num2) {
+		return num1/num2;
+	}
+
+	public static float square(float num1) {
+		return(float) Math.pow(num1, 2);
+	}
+
+	public static float cube(float num1) {
+		return (float) Math.pow( num1, 3 );
+	}
+
+	public static float power(float num1, float num2) {
+		return (float) Math.pow(num1,num2);
+	}
+
+	public static float mod(float num1, float num2) {
+		return num1 % num2;
+	}
+}
